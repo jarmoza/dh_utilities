4dee0ff366a8ffcfe25a5ffabe44df5a987b7f73
Jonathan Armoza <jarmoza@gmail.com>
2019-08-30 00:20:59 -0400

    Fixes for training/validation split, scoring. Addition of verbose options

--- models/logreg.py
+++ models/logreg.py
@@ -23,6 +23,8 @@ class LogReg(object):
 		p_verbose=False):
 
 		# 1. Save parameters for modeling
+		self.m_training_and_validation_data = p_training_and_validation_set
+		self.m_training_and_validation_labels = p_training_and_validation_labels
 		self.m_parameters = {
 
 			"train_test_split": {
@@ -39,8 +41,8 @@ class LogReg(object):
 			}
 		}
 
-		# 2. Save given verbose request across all LogReg instances
-		LogReg.s_verbose = p_verbose
+		# 2. Save given verbosity
+		self.m_verbose = p_verbose
 
 		# 3. Calculate holdout label counts (for holdout prediction scoring), if given
 		self.m_holdout_data = p_holdout_set
@@ -54,17 +56,19 @@ class LogReg(object):
 		self.m_logistic_regression = LogisticRegression(solver=self.m_parameters["fit"]["solver"],
 			multi_class=self.m_parameters["fit"]["multi_class"], max_iter=self.m_parameters["fit"]["max_iter"])
 
-	def fit(self):
+	def fit(self, p_verbose=None):
 
-		if LogReg.s_verbose:
+		verbose = LogReg.verbose_status(p_verbose, self.m_verbose)
+		if verbose:
 			print("Training model...")
 
 		# 1. Fit training data given the labels
 		self.m_logistic_regression.fit(self.m_training_data, self.m_training_labels)
 
-	def holdout_score(self):
+	def holdout_score(self, p_verbose=None, p_print_predictions=False):
 
-		if LogReg.s_verbose:
+		verbose = LogReg.verbose_status(p_verbose, self.m_verbose)
+		if verbose:
 			print("Making predictions of holdout data set...")
 
 		# 1. Make holdout predictions
@@ -76,7 +80,7 @@ class LogReg(object):
 			predicted_labels.append(predicted_label)
 
 			# B. Output labels for respective titles if no holdout labels given
-			if LogReg.s_verbose and None != self.m_holdout_titles:
+			if verbose and None != self.m_holdout_titles and p_print_predictions:
 				print("{0} - Prediction: {1}".format(self.m_holdout_titles[index], predicted_label))
 
 		# 2. Score predictions on holdout set, if holdout labels were given
@@ -89,7 +93,7 @@ class LogReg(object):
 			holdout_label = self.m_holdout_labels[index]
 			
 			# B. Output info about this prediction, if verbose requested
-			if LogReg.s_verbose and None != self.m_holdout_labels:
+			if verbose and None != self.m_holdout_labels and p_print_predictions:
 				if None != self.m_holdout_titles:
 					print("{0} - Label: {1} Prediction: {2}".format(self.m_holdout_titles[index],
 																	holdout_label, predicted_label))
@@ -101,34 +105,39 @@ class LogReg(object):
 				self.m_holdout_prediction_scores[holdout_label] += 1
 
 		# 3. Calculate prediction accuracy scores for each holdout item
+		if verbose:
+			print("Holdout prediction accuracy:")
 		for item_type in self.m_holdout_prediction_scores:
 			self.m_holdout_prediction_scores[item_type] /= float(self.m_holdout_counts[item_type])
 
-			if LogReg.s_verbose:
-				print("{0} Holdout prediction accuracy: {1}%%".format(item_type,
+			if verbose:
+				print("\t{0}: {1}%%".format(item_type,
 					self.m_holdout_prediction_scores[item_type] * 100))
 
 		return self.m_holdout_prediction_scores
 
-	def split_training_and_validation(self):
+	def split_training_and_validation(self, p_verbose=None):
 
-		if LogReg.s_verbose:
+		verbose = LogReg.verbose_status(p_verbose, self.m_verbose)
+		if verbose:
 			print("Splitting up training and validation data sets...")
 
 		# 1. Split the given data set into training and test sets and labels
-		self.m_training_data, self.m_training_labels, self.m_validation_data, self.m_validation_labels = \
-			train_test_split(p_training_and_validation_set, p_training_and_validation_labels,
+		self.m_training_data, self.m_validation_data, self.m_training_labels, self.m_validation_labels = \
+			train_test_split(self.m_training_and_validation_data, self.m_training_and_validation_labels,
 							 test_size=self.m_parameters["train_test_split"]["test_size"],
 							 random_state=self.m_parameters["train_test_split"]["random_state"])
 
 		return self.m_training_data, self.m_training_labels, self.m_validation_data, self.m_validation_labels
 
-	def validation_score(self):
+	def validation_score(self, p_verbose=None):
 
 		# 1. Determine model validation score
-		self.m_validationscore = logistic_regression.score(validation_data, validation_labels)
+		self.m_validation_score = self.m_logistic_regression.score(self.m_validation_data,
+																   self.m_validation_labels)
 		
-		if LogReg.s_verbose:
+		verbose = LogReg.verbose_status(p_verbose, self.m_verbose)
+		if verbose:
 			print("Validation score: {0}".format(self.m_validation_score))
 
 		return self.m_validation_score
@@ -137,4 +146,10 @@ class LogReg(object):
 	@staticmethod
 	def split_data_and_holdouts_half(p_dataset):
 		return p_dataset[0:int(len(p_dataset) / 2.0)], p_dataset[int(len(p_dataset) / 2.0) + 1:]
+
+	@staticmethod
+	def verbose_status(p_given_verbosity, p_instance_verbosity):
+		return p_given_verbosity if None != p_given_verbosity else p_instance_verbosity
+
+
 
